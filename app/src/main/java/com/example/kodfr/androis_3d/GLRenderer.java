cdabf8d6e017004f8611939b31442de4953bd51d
KodFreedom <kodfreedom@gmail.com>
2017-11-21 10:00:52 +0900

    base

--- app/src/main/java/com/example/kodfr/androis_3d/GLRenderer.java
+++ app/src/main/java/com/example/kodfr/androis_3d/GLRenderer.java
@@ -2,6 +2,7 @@ package com.example.kodfr.androis_3d;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+
 import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
 
@@ -9,21 +10,30 @@ import android.opengl.GLSurfaceView.Renderer;
 public class GLRenderer implements Renderer
 {
 	Context context;	// コンテキスト
-
+	
+	Camera camera;		// カメラ
+	Field field;		//　フィールド
 	
 	// コンストラクタ
 	public GLRenderer( Context context )
 	{
 		this.context = context;
+		
+		// ゲームオブジェクト生成
+		camera = new Camera();
+		field = new Field( context );		
 	}
-	
-	
+
 	// サーフェイス生成イベント
 	@Override
 	public void onSurfaceCreated( GL10 gl, EGLConfig config )
 	{
 		// クリア色設定
 	    gl.glClearColor( 0.0f, 0.5f, 0.0f, 1.0f );
+	    
+	    
+	    // ゲームオブジェクト初期化
+	    field.init( gl );
 	}
 	
 	
@@ -33,8 +43,11 @@ public class GLRenderer implements Renderer
 	{
 		// ビューポートサイズ設定
 		gl.glViewport( 0, 0, width, height );
-	}
-
+		
+		// アスペクト比設定
+		camera.setAspect( (float)width / height );
+	}	
+	
 	
 	// 描画イベント
 	@Override
@@ -42,6 +55,17 @@ public class GLRenderer implements Renderer
 	{	
 		// 背景クリア
 	    gl.glClear( GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT );
+	    
+	    
+	    // ゲームオブジェクト描画
+		camera.set( gl );
+		field.draw( gl );
 	}
+
+	
+
+
+	
+
 
 }
